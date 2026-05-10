f5e2337671d5287166e85103d4635b6e5fdffcd0
Karen Payne <paynekaren@comcast.net>
2026-05-10 10:06:12 -0700

    Add int extensions and FullName for Person
    Added IntExtensions.cs with Increment/Decrement methods for int and ref int. Extended PersonEntityExtensions with FullName property. Updated solution to include .editorconfig disabling IDE0022.
    
    by Karen

---
+++ ExtensionsLibrary/IntExtensions.cs
@@ -0,0 +1,25 @@
+﻿namespace ExtensionsLibrary;
+/// <summary>
+/// Provides a set of extension methods for <see cref="int"/> data type.
+/// </summary>
+public static class IntExtensions
+{
+    extension(int number)
+    {
+        public void Increment()
+            => number++;
+        public void Decrement() => number--;
+    }
+
+    /// <summary>
+    /// Provides an extension method container for modifying an <see cref="int"/> value by reference.
+    /// </summary>
+    /// <param name="number">The integer value to be modified by reference.</param>
+    extension(ref int number)
+    {
+        public void RefIncrement()
+            => number++;
+        public void RefDecrement() 
+            => number--;
+    }
+}
--- ExtensionsLibrary/Models/PersonEntityExtensions.cs
+++ ExtensionsLibrary/Models/PersonEntityExtensions.cs
@@ -4,6 +4,9 @@ public static class PersonEntityExtensions
 {
     extension(Person value)
     {
+        /// <summary>
+        /// Gets the full name, consisting of the first name followed by the last name.
+        /// </summary>
        public string FullName => $"{value.Firstname} {value.Lastname}";
     }
 }
