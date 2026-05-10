82ddc137de1642711d9ead7c97462db8106d7d6f
Karen Payne <paynekaren@comcast.net>
2026-05-10 12:50:46 -0700

    Add FieldKeywordSample project demonstrating `field` keyword
    by Karen

---
+++ FieldKeywordSample/Classes/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.CompilerServices;
+
+// ReSharper disable once CheckNamespace
+namespace FieldKeywordSample;
+internal partial class Program
+{
+    [ModuleInitializer]
+    public static void Init()
+    {
+        AnsiConsole.MarkupLine("");
+        Console.Title = "field code sample";
+        WindowUtility.SetConsoleWindowPosition(WindowUtility.AnchorWindow.Center);
+    }
+}
---
+++ FieldKeywordSample/Classes/StringExtensions.cs
@@ -0,0 +1,70 @@
+﻿using System.Text.RegularExpressions;
+
+namespace FieldKeywordSample.Classes;
+public static partial class StringExtensions
+{
+    /// <param name="input">The string to capitalize.</param>
+    extension(string input)
+    {
+        /// <summary>
+        /// Capitalizes the first letter of the given string.
+        /// </summary>
+        /// <returns>A new string with the first letter capitalized. If the input is null or empty, the original string is returned.</returns>
+        public string CapitalizeFirstLetter()
+        {
+            if (string.IsNullOrWhiteSpace(input)) return input;
+
+            return char.ToUpper(input[0]) + input.AsSpan(1).ToString();
+        }
+
+        /// <summary>
+        /// Trims the last character from the given string.
+        /// </summary>
+        /// <returns>A new string with the last character removed, or the original string if it is null or whitespace.</returns>
+        public string TrimLastCharacter()
+            => string.IsNullOrWhiteSpace(input) ?
+                input :
+                input[..^1];
+
+        /// <summary>
+        /// Removes extra spaces from the given string, optionally trimming the end.
+        /// </summary>
+        /// <param name="trimEnd">If set to <c>true</c>, trims the end of the resulting string.</param>
+        /// <returns>A new string with extra spaces removed.</returns>
+        public string RemoveExtraSpaces(bool trimEnd = false)
+        {
+            var result = ExtraSpacesRegex().Replace(input, " ");
+            return trimEnd ? result.TrimEnd() : result;
+        }
+
+        /// <summary>
+        /// Replaces the last occurrence of a specified string within the given string.
+        /// </summary>
+        /// <param name="find">The string to find case-sensitive.</param>
+        /// <param name="replace">The string to replace the found string with.</param>
+        /// <returns>A new string with the last occurrence of the specified string replaced.</returns>
+        public string ReplaceLast(string find, string replace)
+        {
+            var index = input.LastIndexOf(find, StringComparison.OrdinalIgnoreCase);
+            return index == -1 ?
+                input :
+                input.Remove(index, find.Length).Insert(index, replace);
+        }
+    }
+
+
+    /// <summary>
+    /// Provides a regular expression to match sequences of two or more whitespace characters.
+    /// </summary>
+    /// <returns>A <see cref="Regex"/> object that matches sequences of two or more whitespace characters.</returns>
+    /// <remarks>
+    /// Pattern:<br/>
+    /// <code>\s{2,}</code><br/>
+    /// Explanation:<br/>
+    /// <code>
+    /// ○ Match a whitespace character atomically at least twice.<br/>
+    /// </code>
+    /// </remarks>
+    [GeneratedRegex(@"\s{2,}", RegexOptions.None)]
+    private static partial Regex ExtraSpacesRegex();
+}
