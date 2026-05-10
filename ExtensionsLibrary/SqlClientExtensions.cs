802930b85bda56d62f9eb9f2810a0e9c71cd4e39
Karen Payne <paynekaren@comcast.net>
2026-05-09 18:22:59 -0700

    Add async SqlDataReader extensions and refactor class
    by Karen

--- ExtensionsLibrary/SqlClientExtensions.cs
+++ ExtensionsLibrary/SqlClientExtensions.cs
@@ -1,20 +1,46 @@
 ﻿using Microsoft.Data.SqlClient;
 
 namespace ExtensionsLibrary;
-public static class SqlClientExtensions
+
+internal static class SqlClientExtensions
 {
+
     extension(SqlDataReader reader)
     {
         public DateOnly GetDateOnly(int index)
-            => reader.GetFieldValue<DateOnly>(index);
+        => reader.GetFieldValue<DateOnly>(index);
+
+        public async Task<DateOnly> GetDateOnlyAsync(int index)
+            => await reader.GetFieldValueAsync<DateOnly>(index);
+
+        public async Task<DateTime> GetDateTimeAsync(int index)
+            => await reader.GetFieldValueAsync<DateTime>(index);
+
+        public async Task<DateTimeOffset> GetDateTimeOffsetAsync(int index)
+            => await reader.GetFieldValueAsync<DateTimeOffset>(index);
+
+        public async Task<string> GetStringAsync(int index)
+            => await reader.GetFieldValueAsync<string>(index);
+
+        public async Task<int> GetIntAsync(int index)
+            => await reader.GetFieldValueAsync<int>(index);
+
+        public async Task<decimal> GetDecimalAsync(int index)
+            => await reader.GetFieldValueAsync<decimal>(index);
+
+        public async Task<double> GetDoubleAsync(int index)
+            => await reader.GetFieldValueAsync<double>(index);
 
         public TimeOnly GetTimeOnly(int index)
             => reader.GetFieldValue<TimeOnly>(index);
 
-        public string GetTimeOnlyFormatted(int index)
-            => reader.GetFieldValue<TimeOnly>(index).ToString("hh:mm tt");
+        public async Task<TimeOnly> GetTimeOnlyAsync(int index)
+            => await reader.GetFieldValueAsync<TimeOnly>(index);
+    }
 
-        public string GetDateOnlyFormatted(int index)
-            => reader.GetFieldValue<DateOnly>(index).ToString("MM/dd/yyyy");
+    extension(TimeSpan sender)
+    {
+        public TimeOnly ToTimeOnly()
+        => TimeOnly.FromTimeSpan(sender);
     }
 }
