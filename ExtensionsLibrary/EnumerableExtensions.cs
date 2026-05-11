c0878554114ac1c6c392be1de91b91fb8a107a69
Karen Payne <paynekaren@comcast.net>
2026-05-11 12:52:22 -0700

    Add EnumerableExtensions class and minor fix
    Added EnumerableExtensions with Combine and Identity methods (note: uses non-standard C# syntax). Fixed extra brace in EnumExtensions.cs.
    
    by Karen

---
+++ ExtensionsLibrary/EnumerableExtensions.cs
@@ -0,0 +1,22 @@
+﻿namespace ExtensionsLibrary;
+
+public static class EnumerableExtensions
+{
+    extension<TSource>(IEnumerable<TSource>)
+    {
+        /// <summary>
+        /// Combines two sequences into a single sequence by concatenating them.
+        /// </summary>
+        /// <typeparam name="TSource">The type of the elements in the input sequences.</typeparam>
+        /// <param name="first">The first sequence to combine.</param>
+        /// <param name="second">The second sequence to combine.</param>
+        /// <returns>An <see cref="IEnumerable{T}"/> that contains the elements of the first sequence followed by the elements of the second sequence.</returns>
+        public static IEnumerable<TSource> Combine(IEnumerable<TSource> first, IEnumerable<TSource> second)
+            => first.Concat(second);
+
+        // static extension property:
+        public static IEnumerable<TSource> Identity
+            => Enumerable.Empty<TSource>();
+
+    }
+}
