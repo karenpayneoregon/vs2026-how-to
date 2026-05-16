d491472313ec24588697ee29b01faa9122c1e60a
Karen Payne <paynekaren@comcast.net>
2026-05-15 18:36:20 -0700

    format

--- ExtensionsLibrary/TimeSpanExtensions.cs
+++ ExtensionsLibrary/TimeSpanExtensions.cs
@@ -5,7 +5,9 @@ internal static class TimeSpanExtensions
     {
         public string Format(bool debug = true) =>
             debug ? 
-                $"Minutes: {(int)timeSpan.TotalMinutes} Seconds:{timeSpan.Seconds:00} Milliseconds:{timeSpan.Milliseconds:00}" : 
+                $"Minutes: {(int)timeSpan.TotalMinutes} " +
+                $"Seconds:{timeSpan.Seconds:00} " +
+                $"Milliseconds:{timeSpan.Milliseconds:00}" : 
                 $"{(int)timeSpan.TotalMinutes}:{timeSpan.Seconds:00}:{timeSpan.Milliseconds:00}";
     }
 }
