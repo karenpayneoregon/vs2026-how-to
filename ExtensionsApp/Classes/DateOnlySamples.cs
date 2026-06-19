8b045bca485996a24c2f95aa725468f09252eb3d
Karen Payne <paynekaren@comcast.net>
2026-06-19 10:25:43 -0700

    Add XML docs to DateOnlySamples class and methods
    Added detailed XML documentation comments to the DateOnlySamples class and its methods, describing their purpose, usage, and parameters. These comments clarify the use of Spectre.Console for table formatting and CommonLibrary.DateOnlyHelpers for date calculations. No functional changes were made; this update improves code readability and maintainability.
    
    by Karen Payne

--- ExtensionsApp/Classes/DateOnlySamples.cs
+++ ExtensionsApp/Classes/DateOnlySamples.cs
@@ -1,15 +1,26 @@
 ﻿using CommonLibrary;
-using ExtensionsLibrary.Classes;
 using Spectre.Console;
 using SpectreConsoleLibrary.Core;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace ExtensionsApp.Classes;
 
+/// <summary>
+/// Provides methods for displaying date-related information in a formatted table.
+/// </summary>
+/// <remarks>
+/// This class includes methods to display dates for the prior week, current week, next week, 
+/// and a calendar for a specific month. It utilizes the Spectre.Console library for table formatting 
+/// and depends on <see cref="CommonLibrary.DateOnlyHelpers"/> for date calculations.
+/// </remarks>
 internal class DateOnlySamples
 {
+    /// <summary>
+    /// Displays a table of dates for the prior week, starting from the previous Sunday.
+    /// </summary>
+    /// <remarks>
+    /// This method uses <see cref="CommonLibrary.DateOnlyHelpers.PriorWeeksDates"/> to retrieve the dates
+    /// and formats them into a table using Spectre.Console.
+    /// </remarks>
     public static void PriorWeek()
     {
 
@@ -32,6 +43,13 @@ internal class DateOnlySamples
         Console.WriteLine();
     }
 
+    /// <summary>
+    /// Displays a table of dates for the next week, starting from the next occurrence of Sunday.
+    /// </summary>
+    /// <remarks>
+    /// This method retrieves the dates for the next week using <see cref="CommonLibrary.DateOnlyHelpers.NextWeeksDates"/> 
+    /// and displays them in a formatted table using the Spectre.Console library.
+    /// </remarks>
     public static void NextWeek()
     {
 
@@ -54,6 +72,13 @@ internal class DateOnlySamples
         Console.WriteLine();
     }
     
+    /// <summary>
+    /// Displays the dates of the current week in a formatted table.
+    /// </summary>
+    /// <remarks>
+    /// This method retrieves the dates for the current week, starting from Sunday, 
+    /// and displays them in a table format using Spectre.Console.
+    /// </remarks>
     public static void ThisWeek()
     {
         
@@ -72,6 +97,13 @@ internal class DateOnlySamples
         Console.WriteLine();
     }
 
+    /// <summary>
+    /// Displays a calendar table for the specified month in the console.
+    /// </summary>
+    /// <param name="month">
+    /// The month for which the calendar table should be displayed. 
+    /// The value should be an integer between 1 and 12, representing the months January to December.
+    /// </param>
     public static void GetMonthDays(int month)
     {
         SpectreConsoleHelpers.PrintPink();
