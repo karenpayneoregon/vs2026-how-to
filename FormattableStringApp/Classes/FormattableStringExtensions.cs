4d7a5f71893b16509629568141ecd482761d5165
Karen Payne <paynekaren@comcast.net>
2026-07-27 08:35:11 -0700

    Add FormattableStringExtensions for property access
    Added a static FormattableStringExtensions class in FormattableStringApp.Classes with extension methods to get and update Id, FirstName, LastName, and BirthDate properties by manipulating the arguments array of FormattableString.
    
    by Karen Payne

---
+++ FormattableStringApp/Classes/FormattableStringExtensions.cs
@@ -0,0 +1,38 @@
+﻿namespace FormattableStringApp.Classes;
+
+/// <summary>
+/// Provides a set of extension methods for the <see cref="FormattableStringApp.Classes"/> namespace.
+/// </summary>
+public static class FormattableStringExtensions
+{
+
+    extension(FormattableString sender)
+    {
+        public int Id()
+            => Convert.ToInt32(sender.GetArguments()[0]!.ToString());
+
+        public string FirstName()
+            => (string)sender.GetArguments()[1]!;
+
+        public string LastName()
+            => (string)sender.GetArguments()[2]!;
+
+        public DateOnly BirthDate()
+            => (DateOnly)sender.GetArguments()[3]!;
+
+        public void UpdateFirstName(string value)
+        {
+            sender.GetArguments()[1] = value;
+        }
+
+        public void UpdateLastName(string value)
+        {
+            sender.GetArguments()[2] = value;
+        }
+
+        public void UpdateBirthDate(DateOnly value)
+        {
+            sender.GetArguments()[3] = value;
+        }
+    }
+}
