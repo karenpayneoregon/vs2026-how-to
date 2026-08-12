15bbad895f1484cf688fc53f423dc6b5748a4d07
Karen Payne <paynekaren@comcast.net>
2026-08-12 08:20:22 -0700

    extension block

--- ValidateBirthDateApp/LanguageExtensions/RuleBuilderExtensions.cs
+++ ValidateBirthDateApp/LanguageExtensions/RuleBuilderExtensions.cs
@@ -8,33 +8,36 @@ namespace ValidateBirthDateApp.LanguageExtensions;
 /// </summary>
 public static class RuleBuilderExtensions
 {
-    /// <summary>
-    /// Defines a validation rule for a birthdate, ensuring the year is within a valid range.
-    /// </summary>
-    /// <typeparam name="T">The type of the object being validated.</typeparam>
     /// <param name="ruleBuilder">
     /// The <see cref="IRuleBuilder{T, TProperty}"/> used to build the validation rule.
     /// </param>
-    /// <returns>
-    /// An <see cref="IRuleBuilderOptions{T, TProperty}"/> that represents the configured validation rule.
-    /// </returns>
-    /// <remarks>
-    /// The birthdate year must be greater than the year calculated as 120 years before the current year
-    /// and less than or equal to the current year.
-    ///
-    /// Also, consider reading minimum and maximum year values from a configuration file instead of hardcoding them.
-    /// 
-    /// </remarks>
-    public static IRuleBuilderOptions<T, DateOnly> BirthDateRule<T>(this IRuleBuilder<T, DateOnly> ruleBuilder)
+    /// <typeparam name="T">The type of the object being validated.</typeparam>
+    extension<T>(IRuleBuilder<T, DateOnly> ruleBuilder)
     {
+        /// <summary>
+        /// Defines a validation rule for a birthdate, ensuring the year is within a valid range.
+        /// </summary>
+        /// <returns>
+        /// An <see cref="IRuleBuilderOptions{T, TProperty}"/> that represents the configured validation rule.
+        /// </returns>
+        /// <remarks>
+        /// The birthdate year must be greater than the year calculated as 120 years before the current year
+        /// and less than or equal to the current year.
+        /// 
+        /// Also, consider reading minimum and maximum year values from a configuration file instead of hardcoding them.
+        /// 
+        /// </remarks>
+        public IRuleBuilderOptions<T, DateOnly> BirthDateRule()
+        {
 
-        int minYear = DateTime.Now.AddYears(-120).Year;
+            int minYear = DateTime.Now.AddYears(-120).Year;
 
-        return ruleBuilder
-            .Must(x => x.Year > minYear && x.Year <= DateTime.Now.Year)
-            .WithMessage("'{PropertyName}'" +
-                         $" must be greater than {minYear} " +
-                         $"year and less than or equal to {DateTime.Now.Year} ");
+            return ruleBuilder
+                .Must(x => x.Year > minYear && x.Year <= DateTime.Now.Year)
+                .WithMessage("'{PropertyName}'" +
+                             $" must be greater than {minYear} " +
+                             $"year and less than or equal to {DateTime.Now.Year} ");
+        }
     }
 
 }
